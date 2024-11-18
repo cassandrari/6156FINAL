9e7a8684b8d6830d4afb304c1a468788e0809164
cassandrari <criggs1@uncc.edu>
2024-11-18 18:25:00 -0500

    Update sales.py

--- sales.py
+++ sales.py
@@ -7,8 +7,8 @@ import numpy as np
 
 # Load the dataset
 df = pd.read_csv('Superstore_Sales.csv',  encoding='ISO-8859-1') 
-df['Order_Date'] = pd.to_datetime(df['Order_Date'])
-df['Ship_Date'] = pd.to_datetime(df['Ship_Date'])
+df['Order_Date'] = pd.to_datetime(df['Order_Date'], errors='coerce')
+df['Ship_Date'] = pd.to_datetime(df['Ship_Date'], errors='coerce')
 
 
 
