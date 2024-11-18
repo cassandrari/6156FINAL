5e39c59b5bd9e16f303c11aa81a002a6c178011a
cassandrari <criggs1@uncc.edu>
2024-11-18 18:34:11 -0500

    Update sales.py

--- sales.py
+++ sales.py
@@ -6,9 +6,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 # Load the dataset
-df = pd.read_csv('Superstore_Sales.csv',  encoding='ISO-8859-1') 
-df['Order_Date'] = pd.to_datetime(df['Order_Date'], errors='coerce')
-df['Ship_Date'] = pd.to_datetime(df['Ship_Date'], errors='coerce')
+df = pd.read_csv('Superstore_Sales.csv')
 
 
 
